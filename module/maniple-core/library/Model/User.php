f136de0a2482d57211684c36bccfb9a26e2bea77
xemlock <xemlock@gmail.com>
2014-07-04 15:25:12 +0200

    user model update

--- module/maniple-core/library/Model/User.php
+++ module/maniple-core/library/Model/User.php
@@ -2,20 +2,65 @@
 
 class ManipleCore_Model_User extends Maniple_Model_Model implements ManipleCore_Model_UserInterface
 {
+    /**
+     * @var mixed
+     */
     protected $_userId;
 
+    /**
+     * @var string
+     */
     protected $_username;
 
+    /**
+     * @var string
+     */
     protected $_password;
 
+    /**
+     * @var string
+     */
     protected $_email;
 
+    /**
+     * @var mixed
+     */
+    protected $_createdAt;
+
+    /**
+     * @var string
+     * @deprecated
+     */
     protected $_firstName;
 
+    /**
+     * @var string
+     * @deprecated
+     */
     protected $_lastName;
 
     public function getId()
     {
         return $this->_userId;
+    }
+
+    public function getUsername()
+    {
+        return $this->_username;
+    }
+
+    public function getPassword()
+    {
+        return $this->_password;
+    }
+
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->_createdAt;
     }
 }
