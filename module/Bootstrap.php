126582160cf95166f2dfa30c0782acbbad7aa3f5
xemlock <xemlock@gmail.com>
2014-07-16 16:16:02 +0200

    updated module deps in bootstrap

--- module/Bootstrap.php
+++ module/Bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 
-class ManipleCore_Bootstrap extends Maniple_Application_ModuleBootstrap
+class ManipleCore_Bootstrap extends Maniple_Application_Module_Bootstrap
 {
+    protected $_moduleDeps = array('maniple-vendor-assets');
+
     public function onBootstrap(Maniple_Application_ModuleBootstrapper $moduleBootstraper)
     {
-        $moduleBootstraper->bootstrapModule('maniple-vendor-assets');
-
         $bootstrap = $moduleBootstraper->getBootstrap();
 
         if ($bootstrap->hasPluginResource('translate') || method_exists($bootstrap, '_initTranslate')) {
