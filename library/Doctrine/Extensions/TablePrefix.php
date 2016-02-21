57ab45aa2868d747bb90e26c1aa7578d5c90796f
xemlock <xemlock@gmail.com>
2016-02-21 11:27:53 +0100

    auto generate index names when not provided

--- library/Doctrine/Extensions/TablePrefix.php
+++ library/Doctrine/Extensions/TablePrefix.php
@@ -18,6 +18,8 @@ class TablePrefix
         /** @var \Doctrine\ORM\Mapping\ClassMetadata $classMetadata */
         $classMetadata = $eventArgs->getClassMetadata();
 
+        $origTableName = $classMetadata->getTableName();
+
         if (!$classMetadata->isInheritanceTypeSingleTable() || $classMetadata->getName() === $classMetadata->rootEntityName) {
             $classMetadata->setTableName($this->prefix . $classMetadata->getTableName());
         }
@@ -33,11 +35,22 @@ class TablePrefix
         $table = $classMetadata->table;
 
         if (isset($table['indexes'])) {
-            $table['indexes'] = $this->prefixArrayKeys($table['indexes']);
+            $table['indexes'] = $this->prefixIndexes($table['indexes'], 'idx');
+        }
+
+        // create uniqueConstraints from unique columns, so that properly prefixed
+        // constraint names can be generated
+        foreach ($classMetadata->fieldMappings as $field => &$mapping) {
+            if ($mapping['unique']) {
+                $table['uniqueConstraints'][] = array(
+                    'columns' => array($mapping['columnName']),
+                );
+                $mapping['unique'] = false;
+            }
         }
 
         if (isset($table['uniqueConstraints'])) {
-            $table['uniqueConstraints'] = $this->prefixArrayKeys($table['uniqueConstraints']);
+            $table['uniqueConstraints'] = $this->prefixIndexes($table['uniqueConstraints'], 'uniq');
         }
 
         // add prefix to sequence
@@ -49,12 +62,17 @@ class TablePrefix
         $classMetadata->table = $table;
     }
 
-    protected function prefixArrayKeys(array $array)
+    protected function prefixIndexes(array $array, $typePrefix)
     {
         $prefixedArray = array();
 
         foreach ($array as $key => $value) {
-            $prefixedKey = $this->prefix . $key;
+            if (is_int($key)) {
+                // auto gerenerate index name when needed
+                $prefixedKey = $this->_generateIdentifierName($value['columns'], $typePrefix);
+            } else {
+                $prefixedKey = $this->prefix . $key;
+            }
             if (isset($array[$prefixedKey])) {
                 throw new Exception(sprintf('Prefixed name conflict %s', $prefixedKey));
             }
@@ -62,5 +80,14 @@ class TablePrefix
         }
 
         return $prefixedArray;
+    }
+
+    protected function _generateIdentifierName($columnNames, $prefix = '', $maxSize = 30)
+    {
+        $hash = implode("", array_map(function($column) {
+            return dechex(crc32($column));
+        }, $columnNames));
+
+        return substr($this->prefix . $prefix . "_" . $hash, 0, $maxSize);
     }
 }
