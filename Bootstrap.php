92c0e699758ecebdd487bac0a36177f628369b9c
xemlock <xemlock@gmail.com>
2017-04-01 13:51:42 +0200

    Refactor legacy bootstrapping code

--- Bootstrap.php
+++ Bootstrap.php
@@ -2,21 +2,51 @@
 
 class ManipleCore_Bootstrap extends Maniple_Application_Module_Bootstrap
 {
-    protected $_moduleTasks = array('translations');
-
     public function getModuleDependencies()
     {
         return array('maniple-vendor-assets');
     }
 
-    public function onBootstrap(Maniple_Application_ModuleBootstrapper $moduleBootstraper)
+    public function getResourcesConfig()
     {
-        $bootstrap = $moduleBootstraper->getBootstrap();
+        return require dirname(__FILE__) . '/configs/resources.config.php';
+    }
 
-        $bootstrap->bootstrap('request');
-        $bootstrap->bootstrap('view');
+    public function getRoutesConfig()
+    {
+        return require dirname(__FILE__) . '/configs/routes.config.php';
+    }
 
-        $view = $bootstrap->getResource('view');
+    public function getTranslationsConfig()
+    {
+        return array(
+            'scan'    => Zend_Translate::LOCALE_DIRECTORY,
+            'content' => dirname(__FILE__) . '/languages',
+        );
+    }
+
+    public function getViewConfig()
+    {
+        return array(
+            'helperPaths' => array(
+                'ManipleCore_View_Helper_' => dirname(__FILE__) . '/library/View/Helper/',
+            ),
+        );
+    }
+
+    public function getAssetsBaseDir()
+    {
+        return 'core';
+    }
+
+    protected function _initView()
+    {
+        $application = $this->getApplication();
+
+        $application->bootstrap('request');
+        $application->bootstrap('view');
+
+        $view = $application->getResource('view');
         $view->headScript()->appendScript(sprintf(
             'require.config(%s)',
             Zefram_Json::encode(
@@ -31,34 +61,5 @@ class ManipleCore_Bootstrap extends Maniple_Application_Module_Bootstrap
                 )
             )
         ));
-    }
-
-    public function getAssetsBaseDir()
-    {
-        return 'core';
-    }
-
-    public function getResourcesConfig()
-    {
-        return require dirname(__FILE__) . '/configs/resources.config.php';
-    }
-
-    protected function _initRouter()
-    {
-        /** @var Zend_Controller_Router_Rewrite $router */
-        $router = $this->getApplication()->getResource('FrontController')->getRouter();
-        $router->addConfig(new Zend_Config(
-            require dirname(__FILE__) . '/configs/routes.config.php'
-        ));
-    }
-
-    protected function _initView()
-    {
-        $bootstrap = $this->getApplication();
-        $bootstrap->bootstrap('View');
-
-        /** @var Zend_View $view */
-        $view = $bootstrap->getResource('View');
-        $view->addHelperPath(dirname(__FILE__) . '/library/View/Helper/', 'ManipleCore_View_Helper_');
     }
 }
